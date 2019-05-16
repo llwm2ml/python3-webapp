324daa0221d4161a6a0b383068bf354115888b91
lvwei <lvwei@bogaosoft.com>
2019-05-16 17:57:58 +0800

    edit

---
+++ test.py
@@ -0,0 +1,11 @@
+def aa():
+    from collections import deque
+    q = deque([1, 0])
+    while True:
+        while True:
+            item = q.popleft()
+            q.append(item + q[0])
+            if not q[0]:
+                break
+        q.append(0)
+        yield (list(q)[1:-1])
